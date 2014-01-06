b6e0100981bebc99eada3ef71424efd02248c7b5
dabreaks-mac <k.j.derosa@gmail.com>
2014-01-06 08:48:30 -0500

    initial load from mac
    Hello World, classes and interfaces using Bicycle objects

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication2;
+
+/**
+ *
+ * @author kylederosa
+ */
+
+
+
+
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+      public static void main(String[] args) {
+
+        // Create two different 
+        // Bicycle objects
+        Bicycle bike1 = new Bicycle();
+        MountainBike bike2 = new MountainBike();
+        ACMEBicycle bike3 = new ACMEBicycle();
+        
+        // Invoke methods on 
+        // those objects
+        bike1.changeCadence(50);
+        bike1.speedUp(10);
+        bike1.changeGear(2);
+        bike1.printStates();
+
+        bike2.changeCadence(50);
+        bike2.speedUp(10);
+        bike2.changeGear(2);
+        bike2.changeCadence(40);
+        bike2.speedUp(10);
+        bike2.changeGear(3);
+        bike2.printStates();
+        
+        bike3.changeCadence(50);
+        bike3.speedUp(30);
+        bike3.changeGear(2);
+        bike3.printStates();
+    }
+}
---
+++ JavaApplication2/src/javaapplication2/MountainBike.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication2;
+
+/**
+ *
+ * @author kylederosa
+ */
+class MountainBike extends Bicycle {
+
+    // new fields and methods defining 
+    // a mountain bike would go here
+
+    void printStates() {
+         System.out.println(
+             "cadence:" + cadence + 
+             " speed:" + speed + 
+             " gear:" + gear +
+             " MOUNTAIN BIKE!!");
+    }
+    
+}
